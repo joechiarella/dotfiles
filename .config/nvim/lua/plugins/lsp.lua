10cd4229d8b334691a6460f74077f736e438095b
Joe Chiarella <joe@adpipe.com>
2024-01-27 20:14:31 -0500

    updated elixir lsp handling

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,31 +1,60 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        solargraph = {
-          mason = false,
-          cmd = { "bundle", "exec", "solargraph", "stdio" },
-          flags = {
-            debounce_text_changes = 150,
-          },
-          solargraph = {
-            diagnostics = true,
-          },
-        },
-        elixirls = {
-          mason = false,
-          settings = {
-            elixirLS = {
-              dialyzerEnabled = false,
-            },
-          },
-          cmd = { "/Users/joechiarella/.lsp/elixir-ls/language_server.sh" },
-        },
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				solargraph = {
+					mason = false,
+					cmd = { "bundle", "exec", "solargraph", "stdio" },
+					flags = {
+						debounce_text_changes = 150,
+					},
+					solargraph = {
+						diagnostics = true,
+					},
+				},
+				-- elixirls = {
+				-- 	mason = false,
+				-- 	settings = {
+				-- 		elixirLS = {
+				-- 			dialyzerEnabled = false,
+				-- 		},
+				-- 	},
+				-- 	cmd = { "/Users/joe/.lsp/elixir-ls/language_server.sh" },
+				-- },
+			},
+		},
+	},
+	{
+		"elixir-tools/elixir-tools.nvim",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local elixir = require("elixir")
+			local elixirls = require("elixir.elixirls")
+
+			elixir.setup({
+				nextls = { enable = false },
+				credo = {},
+				elixirls = {
+					enable = true,
+					settings = elixirls.settings({
+						dialyzerEnabled = false,
+						enableTestLenses = false,
+					}),
+					on_attach = function(client, bufnr)
+						vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+						vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+						vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+					end,
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 }
